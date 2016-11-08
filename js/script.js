1be9a1bfdc1336b0d330e046f0ceef8bfa600f81
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-11-08 19:41:32 +0400

    add template

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by hunter_s70 on 31.10.2016.
+ */
+var app = angular.module('TicTac', []);
+
+app.controller('TicTacCtrl', function($scope) {
+    $scope.pl1 = 'X';
+    $scope.pl2 = 'O';
+});
+
+app.directive('ticTac', function() {
+    return {
+        restrict : "E",
+        templateUrl : 'template/field.html',
+        link     : function (scope, element, attrs) {
+            scope.step = function () {
+                alert('step');
+            }
+        }
+    }
+});
--- js/tic-tac.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by hunter_s70 on 31.10.2016.
- */
-alert('2');
