33ee2a334d192d795dd281f2b61d125ef0516c23
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-11-10 20:28:31 +0400

    identify rows and cols

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,7 @@
 app.controller('TicTacCtrl', function($scope) {
     $scope.pl1 = 'X';
     $scope.pl2 = 'O';
+    $scope.step = '';
     $scope.rows = [0, 1, 2];
     $scope.cols = $scope.rows;
 });
--- js/directive.js
+++ js/directive.js
@@ -3,8 +3,8 @@ app.directive('ticTac', function() {
         restrict : "E",
         templateUrl : 'template/field.html',
         link     : function (scope, element, attrs) {
-            scope.makeStep = function () {
-                console.log(attrs);
+            scope.makeStep = function (row, col) {
+                console.log(row, col);
             };
         }
     }
