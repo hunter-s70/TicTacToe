5bf6fc71d5e845f4b23bbd99103488da579519be
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-11-13 13:00:45 +0400

    add click on cells, toggle player

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,12 @@
 app.controller('TicTacCtrl', function($scope) {
-    $scope.pl1 = 'X';
-    $scope.pl2 = 'O';
-    $scope.step = '';
+    $scope.pl1 = 'x';
+    $scope.pl2 = '​o';
+    $scope.currentPlayer = $scope.pl1;
     $scope.rows = [0, 1, 2];
     $scope.cols = $scope.rows;
+    $scope.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
 });
--- js/directive.js
+++ js/directive.js
@@ -4,8 +4,30 @@ app.directive('ticTac', function() {
         templateUrl : 'template/field.html',
         link     : function (scope, element, attrs) {
             scope.makeStep = function (row, col) {
+                if (scope.currentPlayer === scope.pl1) {
+                    scope.value = scope.pl1;
+                    scope.currentPlayer = scope.pl2;
+                } else {
+                    scope.value = scope.pl2;
+                    scope.currentPlayer = scope.pl1;
+                }
+
+                scope.setCell(row, col, scope.value);
                 console.log(row, col);
             };
+
+            scope.viewStep = function (row, col) {
+                scope.value = scope.getCell(row, col);
+                return scope.value ? scope.value : '';
+            };
+
+            scope.getCell = function (row, col) {
+                return scope.board[row][col];
+            };
+
+            scope.setCell = function (row, column, value) {
+                scope.board[row][column] = value;
+            }
         }
     }
 });
