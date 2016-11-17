4b269f1daf1857ba2f4312f26b27ba87908b01c9
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-11-17 23:16:27 +0400

    make multiple colScore object

--- js/controller.js
+++ js/controller.js
@@ -15,21 +15,23 @@ app.controller('TicTacCtrl', function($scope) {
         $scope.countColScore = {};
 
         for (var row in $scope.board) {
+            $scope.countColScore[row] = {};
             $scope.countRowScore[row] = 0;
 
             for (var col in $scope.board[row]) {
-                $scope.countColScore[col] = 0;
+                $scope.countColScore[row][col] = 0;
 
                 if ($scope.board[row][col] !== null  && $scope.board[row][col] !== $scope.currentPlayer) {
                     $scope.countRowScore[row] = +col + $scope.countRowScore[row];
-                    $scope.countColScore[col] = +col + $scope.countColScore[col];
+                    $scope.countColScore[row][col] = +col + $scope.countColScore[row][col];
                 }
+
                 if ($scope.countRowScore[row] === 6) {
                     alert('win');
                 }
             }
-            console.log($scope.countColScore);
         }
+        console.log($scope.countColScore);
         console.log($scope.countRowScore);
     };
 });
