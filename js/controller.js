9634dae45519f60297dba6689e77222ad0f2f4af
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-11-18 20:59:49 +0400

    add check for columns score && add comments

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,9 @@
+//Points Table
+//  1 | 2 | 3
+// ---|---|---
+//  1 | 2 | 3
+// ---|---|---
+//  1 | 2 | 3
 app.controller('TicTacCtrl', function($scope) {
     $scope.pl1 = 'x';
     $scope.pl2 = '​o';
@@ -13,7 +19,12 @@ app.controller('TicTacCtrl', function($scope) {
     $scope.isWinner = function () {
         $scope.countRowScore = {};
         $scope.countColScore = {};
+        $scope.colScoreSumm = {};
 
+        //loop for count row score
+        //if player put value in first column  --> 1 point
+        //if player put value in second column --> 2 point
+        //if player put value in third column  --> 3 point
         for (var row in $scope.board) {
             $scope.countColScore[row] = {};
             $scope.countRowScore[row] = 0;
@@ -21,17 +32,40 @@ app.controller('TicTacCtrl', function($scope) {
             for (var col in $scope.board[row]) {
                 $scope.countColScore[row][col] = 0;
 
+                //check if player put value on game board
                 if ($scope.board[row][col] !== null  && $scope.board[row][col] !== $scope.currentPlayer) {
                     $scope.countRowScore[row] = +col + $scope.countRowScore[row];
                     $scope.countColScore[row][col] = +col + $scope.countColScore[row][col];
                 }
 
+                //if player have 6 score in row
                 if ($scope.countRowScore[row] === 6) {
                     alert('win');
                 }
             }
         }
-        console.log($scope.countColScore);
-        console.log($scope.countRowScore);
+
+        //count points in columns
+        //if player put value in first column  --> 1 point
+        //if player put value in second column --> 2 point
+        //if player put value in third column  --> 3 point
+        for (var row in $scope.countColScore) {
+            $scope.colScoreSumm[row] = 0;
+            for (var col in $scope.countColScore[row]) {
+                $scope.colScoreSumm[row] += $scope.countColScore[col][row];
+            }
+
+            //check if player have
+            // 3 points in first column
+            // 6 points in second column
+            // 9 points in third column
+            if ($scope.colScoreSumm[row] === row*3) {
+                alert('win');
+            }
+        }
+
+//        console.log($scope.countColScore);
+        console.log($scope.colScoreSumm);
+//        console.log($scope.countRowScore);
     };
 });
