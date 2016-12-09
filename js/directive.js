bc642f2b4ac08c2090aa0e1d05ffb4257a67b06c
hunter-s70 <serg.buhtoyarov@gmail.com>
2016-12-09 20:42:47 +0400

    make computer

--- js/directive.js
+++ js/directive.js
@@ -12,14 +12,17 @@ app.directive('ticTac', function($timeout) {
                 scope.isWinner();
             };
 
+
             scope.viewStep = function (row, col) {
                 scope.value = scope.getCell(row, col);
                 return scope.value ? scope.value : '';
             };
 
+
             scope.getCell = function (row, col) {
                 return scope.board[row][col];
             };
+
 
             scope.setCell = function (row, col, value) {
                 if (!scope.board[row][col]) {
@@ -27,53 +30,52 @@ app.directive('ticTac', function($timeout) {
                 }
             };
 
+
             scope.makeStepComputer = function () {
                 if (scope.computerEnable && scope.currentPlayer === scope.pl2) {
                     $timeout(function() {
                         var row, col;
 
-                        console.log(scope.countResults);
-
-                        scope.allCountersCheck = scope.countResults.some(function (counter) {
-                            return counter === 2;
-                        });
+                        console.log('countRes --> ' + scope.countResults);
+                        console.log('CPU__Res --> ' + scope.countComputerResults);
 
                         do {
                             row = scope.makeRandom(0, 2);
                             col = scope.makeRandom(0, 2);
                         } while (scope.board[row][col] !== null);
 
-                        if (scope.allCountersCheck) {
+                        var allCountersCheck = scope.countResults.some(function (counter) {
+                            return counter === 2;
+                        });
+
+                        if (allCountersCheck) {
                             scope.countResults.forEach(function (item, i) {
-                                //for rows
-                                if (item === 2 && i > 4) {
-                                    do {
-                                        row = i - 5;
-                                        col = scope.makeRandom(0, 2);
-                                    } while (scope.board[row][col] === scope.pl1.simbol)
-                                }
 
-                                //for cols
-                                if (item === 2 && i > 1 && i < 5) {
-                                    do {
-                                        row = scope.makeRandom(0, 2);
+                                for (var n = 0; n < 3; n++) {
+                                    if (item !== 2) return;
+
+                                    //for diagonal
+                                    if (i === 0 && scope.board[n][n] === null) {
+                                        row = col = n;
+                                    }
+
+                                    //for diagonal reflect
+                                    if (i === 1 && scope.board[n][2 - n] === null) {
+                                        row = n;
+                                        col = 2 - n;
+                                    }
+
+                                    //for cols
+                                    if (i > 1 && i < 5 && scope.board[n][i - 2] === null) {
+                                        row = n;
                                         col = i - 2;
-                                    } while (scope.board[row][col] === scope.pl1.simbol)
-                                }
+                                    }
 
-                                //for diagonal
-                                if (item === 2 && i === 0) {
-                                    do {
-                                        row = col = scope.makeRandom(0, 2);
-                                    } while (scope.board[row][col] === scope.pl1.simbol)
-                                }
-
-                                //for diagonal reflect
-                                if (item === 2 && i === 1) {
-                                    do {
-                                        row = scope.makeRandom(0, 2);
-                                        col = 2 - row;
-                                    } while (scope.board[row][col] === scope.pl1.simbol)
+                                    //for rows
+                                    if (i > 4 && scope.board[i - 5][n] === null) {
+                                        row = i - 5;
+                                        col = n;
+                                    }
                                 }
                             })
                         }
@@ -83,10 +85,12 @@ app.directive('ticTac', function($timeout) {
                 }
             };
 
+
             scope.makeRandom = function (min, max) {
                 var rand = min - 0.5 + Math.random() * (max - min + 1);
                 return rand = Math.abs( Math.round(rand) );
             };
+
 
             //add watchers for board rows
             scope.board.forEach(function(row, i) {
