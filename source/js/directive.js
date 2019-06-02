a68a920c32b2833204023fb40bcdb890dc89645f
hunter-s70 <serg.buhtoyarov@gmail.com>
2019-06-02 13:41:46 +0300

    upd for redeploy

--- source/js/directive.js
+++ source/js/directive.js
@@ -1,8 +1,8 @@
 app.directive('ticTac', function($timeout) {
     return {
-        restrict : "E",
-        templateUrl : 'template/field.html',
-        link     : function (scope, element, attrs) {
+        restrict: "E",
+        templateUrl: 'template/field.html',
+        link: function (scope, element, attrs) {
 
             scope.makeStep = function (row, col) {
                 if (scope.getCell(row, col)) return;
